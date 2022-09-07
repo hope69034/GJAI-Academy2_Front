e6abb781d6d095a8a325128cc0a14166cd5b7306
sehyoungchoi <hope69034@gmail.com>
2022-09-07 17:12:21 +0900

    907_5_12

--- 03.JavaScript/ch03.조건문/practice.js
+++ 03.JavaScript/ch03.조건문/practice.js
@@ -1,7 +1,7 @@
 // 1번 문제 생년월일을입력받아 만나이 계산
 
 
-function pp(birthday) {
+/* function pp(birthday) {
 
     const birth = new Date(birthday)//생일날짜
     const now = new Date() //현날짜
@@ -9,6 +9,19 @@ function pp(birthday) {
     const m = (now.getMonth()+1) - (birth.getMonth()+1) //양수면생일지남 음수면 생일안지남 0이면 판단이 필요함 
  
     if (m < 0 ||   ( m===0  && now.getDate < birth.getDate   )  )  {    
+ //생일이 안지났으면 -1
+    return year -1
+}
+return year
+}
+console.log(pp("1987-03-31")) */
+
+
+function pp(birthday) {
+    const year = new Date().getFullYear() - new Date(birthday).getFullYear() //현year-생일year = 만나이
+    const m = (new Date().getMonth()+1) - (new Date(birthday).getMonth()+1) 
+    //현month-생일month > 양수면생일지남 음수면 생일안지남 0이면 판단이 필요함 
+    if (m < 0 ||   ( m===0  && new Date().getDate < new Date(birthday).getDate   )  )  {    
  //생일이 안지났으면 -1
     return year -1
 }
